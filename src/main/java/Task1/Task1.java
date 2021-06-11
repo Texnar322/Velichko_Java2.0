1669bebb0e048cc77918508b3fac2b3477c828cf
Texnar322 <zheka201589@gmail.com>
2021-06-11 13:01:09 +0300

    Add Task

--- src/main/java/Task1/Task1.java
+++ src/main/java/Task1/Task1.java
@@ -1,5 +1,33 @@
 package Task1;
 
-public class Task1 {
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) {
+        /*
+
+         */
+        Scanner scanner = new  Scanner(System.in);
+        System.out.println("Выберите тип операции: -, +, *, /");
+        String sym = scanner.nextLine();
+        System.out.println("Введите первое число");
+        double x = scanner.nextDouble();
+        System.out.println("Введите второе число");
+        double y = scanner.nextDouble();
+        double z = switch(sym)
+         {
+            case "+" -> x+y;
+            case "-" -> x-y;
+            case "*" -> x*y;
+            case "/" -> x/y;
+             default -> throw new IllegalStateException("Unexpected value: " + sym);
+         };
+        System.out.format("%.4f",z);
+
+
+
+
+
+
+    }
 }
