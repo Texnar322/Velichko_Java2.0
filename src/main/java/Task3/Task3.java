6dccbe2ff3ab77354a168cbd2e5a600742792c65
Texnar322 <zheka201589@gmail.com>
2021-06-20 18:04:31 +0300

    add 4 Tasks

--- src/main/java/Task3/Task3.java
+++ src/main/java/Task3/Task3.java
@@ -5,17 +5,33 @@ public class Task3
     //1. Создать несколько объектов с параметрами т.е переменными вес цена  название уник параметр типо цвет
     public static void main(String[] args)
     {
-            Gift candy = new Gift();
-            candy.displayInfo();
-
+        Candy candy = new Candy();
+        candy.displayInfo();
+        Jellybean jellybean = new Jellybean();
+        jellybean.displayInfo();
+        Chocolate chocolate = new Chocolate();
+        chocolate.displayInfo();
     }
-
-
 }
-class Gift {
+
+class Candy {
         String ves = "2000грамм.";
         void displayInfo()
         {
             System.out.println(ves);
         }
+}
+class Jellybean {
+    String ves = "2000грамм.";
+    void displayInfo()
+    {
+        System.out.println(ves);
+    }
+}
+class Chocolate {
+    String ves = "2000грамм.";
+    void displayInfo()
+    {
+        System.out.println(ves);
+    }
 }
