55c943b8f732b6fe6ae3f74fd69d1c3145a1dd9f
Texnar322 <zheka201589@gmail.com>
2021-06-28 19:16:26 +0300

    Fix

---
+++ src/main/java/Task4/Arifmetic.java
@@ -0,0 +1,17 @@
+package Task4;
+
+public class Arifmetic {
+
+//    int y;
+//    int x;
+//    String sym;
+//    int z = switch(sym)
+//            {
+//                case "+" -> x+y;
+//                case "-" -> x-y;
+//                case "*" -> x*y;
+//                case "/" -> x/y;
+//                default -> throw new IllegalStateException("Unexpected value: " + sym);
+//            };
+
+}
--- src/main/java/Task4/Task4.java
+++ src/main/java/Task4/Task4.java
@@ -1,7 +1,14 @@
 package Task4;
 
-public class Task4 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Task4 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введите выражение для вычисления:");
+        Reader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String name = bufferedReader.readLine();
+
+        System.out.println(name);
     }
 }
